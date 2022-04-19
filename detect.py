1b16fe6edfbac7543f03e474ec985e12baa76e02
Jin <95178395+jkd2021@users.noreply.github.com>
2022-04-19 13:24:02 +0200

    Update detect.py

--- detect.py
+++ detect.py
@@ -169,7 +169,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', nargs='+', type=str, default='yolov5s.pt', help='model.pt path(s)')
     # parser.add_argument('--source', type=str, default='0', help='source')
-    parser.add_argument('--source', type=str, default=r'D:\Video_Disk_D\Japan_Drive_01.mp4', help='source')
+    parser.add_argument('--source', type=str, default='runs/video/Drive_01.mp4', help='source')
     #                         # (0 for webcam)!!!!!!!
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.7, help='object confidence threshold')
